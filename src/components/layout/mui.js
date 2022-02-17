c27219c823cca4c797cd4fafa4c526b911d0c3f4
Yeghishe Hovhannisyan <yeghishex.hovhannisyan@winfinity.live>
2022-02-17 15:52:35 +0400

    add content styles

--- src/components/layout/mui.js
+++ src/components/layout/mui.js
@@ -5,5 +5,9 @@ export const ContainerSC = styled(Container)`
 `;
 
 export const ContentSC = styled(Box)`
-  
+    background: #212330c7;
+    padding: 20px;
+    border-radius: 20px;
+    color: #EDEDEF;
+    box-shadow: inset #373b3e 0 0 40px 10px, #303538 0 0 24px 12px;
 `;
